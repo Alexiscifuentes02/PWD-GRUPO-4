a53df0dd4179b3ff627c36dd8f7e72cd456a36a3
gastonllaupe <llaupeg@gmail.com>
2025-09-01 16:56:56 -0300

    boton volver al menu en pie

--- TP1/Vista/Estructura/Pie.php
+++ TP1/Vista/Estructura/Pie.php
@@ -1,15 +1,20 @@
-    <footer id="pie" class="bg-dark text-light text-center py-3 mt-4">
-        <p class="mb-1"><h4>INTEGRANTES</h4></p>
-        <ul id="integrantes" class="list-inline mb-0">
-            <li class="list-inline-item"><h5>Alexis Cifuentes</h5></li>
-            <li class="list-inline-item"><h5>Lucas Gabriel Cerda</h5></li>
-            <li class="list-inline-item"><h5>Carlos Curilef</h5></li>
-            <li class="list-inline-item"><h5>Gaston Llaupe</h5></li>
-        </ul>
-        <small class="d-block mt-2 text-secondary">
-            <h6>©2025 Copyright</h6>
-        </small>
-    </footer>
+<footer id="pie" class="bg-dark text-light text-center py-3 mt-4">
+    <p class="mb-1"><h4>INTEGRANTES</h4></p>
+    <ul id="integrantes" class="list-inline mb-0">
+        <li class="list-inline-item"><h5>Alexis Cifuentes</h5></li>
+        <li class="list-inline-item"><h5>Lucas Gabriel Cerda</h5></li>
+        <li class="list-inline-item"><h5>Carlos Curilef</h5></li>
+        <li class="list-inline-item"><h5>Gaston Llaupe</h5></li>
+    </ul>
+
+    <!-- Link para volver al menú -->
+    <div class="mt-3">
+        <a href="../../index.php" class="btn btn-outline-light">Volver al menú</a>
+    </div>
+
+    <small class="d-block mt-2 text-secondary">
+        <h6>©2025 Copyright</h6>
+    </small>
+</footer>
 </body>
 </html>
-
