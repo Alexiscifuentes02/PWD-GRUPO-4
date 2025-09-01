8da90d54b3f8834fe328379293856c7519ee1a91
gastonllaupe <llaupeg@gmail.com>
2025-09-01 14:21:55 -0300

    index agregado

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Trabajos Prácticos - Programación Web Dinámica</title>
+
+  <!-- Bootstrap referenciado para que no de problemas-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<?php include_once("TP1/Vista/Estructura/Encabezado.php"); ?>
+
+<main class="container my-5 text-center">
+    <!-- TRABAJO PRÁCTICO 1 -->
+    <section class="mb-5">
+        <h2 class="mb-4">Trabajo Práctico 1</h2>
+        <ul class="list-group d-inline-block text-start">
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 1</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 2</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 3</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 4</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 5</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 6</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 7</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 8</a></li>
+        </ul>
+    </section>
+
+    <!-- TRABAJO PRÁCTICO 2 -->
+    <section class="mb-5">
+        <h2 class="mb-4">Trabajo Práctico 2</h2>
+        <ul class="list-group d-inline-block text-start">
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 1</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 2</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 3</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 4</a></li>
+        </ul>
+    </section>
+
+    <!-- TRABAJO PRÁCTICO 3 -->
+    <section class="mb-5">
+        <h2 class="mb-4">Trabajo Práctico 3</h2>
+        <ul class="list-group d-inline-block text-start">
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 1</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 2</a></li>
+            <li class="list-group-item"><a href="poner ruta al archivo aca">Ejercicio 3</a></li>
+        </ul>
+    </section>
+</main>
+
+<?php include_once("TP1/Vista/Estructura/Pie.php"); ?>
+
+</body>
+</html>
